b75b4d1a6b24d5a42a952a3e60576f0d2ce733ab
a.bordiug <a.bordiug@gmail.com>
2023-11-05 16:47:40 +0200

    init

---
+++ hw_01/hw-01.js
@@ -0,0 +1,4 @@
+let userName = prompt('What is your name?');
+alert(`Hello, ${userName}! How are you?`);
+
+
---
+++ hw_02/hw_02.js
@@ -0,0 +1,7 @@
+let numberA = Number(prompt('Enter first number'));
+let numberB = Number(prompt('Enter second number'));
+let numberC = Number(prompt('Enter third number'));
+let res = (numberA + numberB + numberC) / 3;
+
+alert(`${numberA} + ${numberB} + ${numberC} / 3 = ${res}`);
+
