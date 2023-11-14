a1b9eafef37a8cb3a589b3311bd514ff90c95d26
a.bordiug <a.bordiug@gmail.com>
2023-11-14 21:00:45 +0200

    git commit -m "updates"

--- hw_05/hw_05.js
+++ hw_05/hw_05.js
@@ -1,15 +1,21 @@
 let arr = [];
 let arrayLength;
 
-if (arrayLength !== null) {
-  do {
-      arrayLength = prompt('Set array length');
-  } while ((isNaN(arrayLength)));
-};
+do {
+  arrayLength = Math.floor(prompt('Set array length'));
+} while (isNaN(arrayLength));
+console.log(arrayLength);
 
 for (let i = 0; i < arrayLength; i++) {
   let arrayElem = prompt('Set array element #' + (i + 1));
-  arr.push(arrayElem);
+  if (arrayElem !== null) {
+    arr.push(arrayElem);
+  } else {
+    alert(
+      `You cancel to enter elements, array length will be shorter than ${arrayLength}`,
+    );
+    break;
+  }
 }
 console.log(arr);
 
@@ -19,4 +25,4 @@ let arrSort = arr.sort(function (a, b) {
 console.log(arrSort);
 
 let arrCut = arr.splice(1, 3);
-console.log(arrCut);
+console.log(arr);
