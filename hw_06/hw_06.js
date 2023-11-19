89118fc5f62946d852b8b6ea4fa56fc111e315aa
a.bordiug <a.bordiug@gmail.com>
2023-11-19 16:12:21 +0200

    updates

--- hw_06/hw_06.js
+++ hw_06/hw_06.js
@@ -71,4 +71,50 @@ function doMath(x, znak, y) {
 
 // Написати функцію заповнення даними користувача двомірного масиву. Довжину основного масиву і внутрішніх масивів задає користувач. Значення всіх елементів всіх масивів задає користувач.
 
+let mainArrayLength = getArrayLength('Set length of main array');
+let innerArrayLength = getArrayLength('Set length of inner array');
+
+console.log(fillArray(mainArrayLength, innerArrayLength));
+
+function fillArray(mainArrLength, innerArrLength) {
+  let arr = [];
+  for (let i = 0; i < mainArrLength; i++) {
+    let innerArr = [];
+    for (let j = 0; j < innerArrLength; j++) {
+      const value = prompt(
+        `Enter value of inner array ${i + 1}, index ${j + 1 - 1}:`,
+      );
+      innerArr.push(value);
+    }
+    arr.push(innerArr);
+  }
+  return arr;
+}
+
+function getArrayLength(arrayLengthMessage) {
+  let array;
+  do {
+    if (array !== null) {
+      array = Math.floor(prompt(arrayLengthMessage));
+    }
+  } while (isNaN(array));
+  return array;
+}
+
 // Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". Вихідний рядок та символи для видалення задає користувач.
+
+const inputStroke = prompt('Enter text');
+const symbolsToRemove = prompt('Enter letters you want to remove');
+
+console.log(removeElement(inputStroke, symbolsToRemove));
+
+function removeElement(input, elementsToRemove) {
+  let inputElement = input.split('');
+  for (let i = 0; i < inputElement.length; i++) {
+    if (elementsToRemove.includes(inputElement[i])) {
+      inputElement.splice(i, 1);
+      i--;
+      };
+  }
+    return inputElement.join('')
+}
