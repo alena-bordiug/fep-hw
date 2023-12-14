fcffa3fa724a311c2c98cbbba1ef079eb71ea5d3
a.bordiug <a.bordiug@gmail.com>
2023-12-14 12:17:07 +0200

    dynamic table create

---
+++ hw_11/table.js
@@ -0,0 +1,8 @@
+// Вивести таблицю 10 × 10, заповнену числами від 1 до 100 (таблиця створюється динамічно)
+
+const table = document.createElement('table')
+table.classList.add('js-table-style');
+
+
+
+document.getElementById('js-table').appendChild(table);
